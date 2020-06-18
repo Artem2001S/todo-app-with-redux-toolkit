6a40c1d1749e60151de7f1cb2075b0372905f6e1
Artem Kazaryan <artem.kazaryan.tema@gmail.com>
2020-06-18 17:44:19 +0300

    use createSlice

--- src/redux/reducers/todosReducer.js
+++ src/redux/reducers/todosReducer.js
@@ -1,4 +1,4 @@
-import { createReducer, createAction } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   todos: [],
@@ -6,29 +6,34 @@ const initialState = {
 
 let nextTodoId = 0;
 
-export const addTodo = createAction('ADD_TODO', (todoText) => {
-  return {
-    payload: { id: nextTodoId++, todoText },
-  };
+const todosSlice = createSlice({
+  name: 'todos',
+  initialState,
+  reducers: {
+    addTodo: {
+      reducer(state, { payload }) {
+        state.todos.push({
+          id: payload.id,
+          todoText: payload.todoText,
+          isDone: false,
+        });
+      },
+      prepare(todoText) {
+        return { payload: { id: nextTodoId++, todoText } };
+      },
+    },
+
+    deleteTodo: (state, { payload }) => {
+      state.todos = state.todos.filter((todo) => todo.id !== payload);
+    },
+
+    toggleTodo: (state, { payload }) => {
+      state.todos = state.todos.map((todo) =>
+        todo.id === payload ? { ...todo, isDone: !todo.isDone } : todo
+      );
+    },
+  },
 });
 
-export const deleteTodo = createAction('DELETE_TODO');
-export const toggleTodo = createAction('TOGGLE_TODO');
-
-export default createReducer(initialState, {
-  [addTodo]: (state, { payload }) => {
-    state.todos.push({
-      id: payload.id,
-      todoText: payload.todoText,
-      isDone: false,
-    });
-  },
-  [deleteTodo]: (state, { payload }) => {
-    state.todos = state.todos.filter((todo) => todo.id !== payload);
-  },
-  [toggleTodo]: (state, { payload }) => {
-    state.todos = state.todos.map((todo) =>
-      todo.id === payload ? { ...todo, isDone: !todo.isDone } : todo
-    );
-  },
-});
+export default todosSlice.reducer;
+export const { addTodo, deleteTodo, toggleTodo } = todosSlice.actions;
